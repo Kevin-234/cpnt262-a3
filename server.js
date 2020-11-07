120eb63a6ac59f948f0527b248a1267f0a90437e
Akari Taguchi <kevintaguchi0@gmail.com>
2020-11-06 22:40:21 -0700

    fixed server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const express = require('express');
-const app = express();
 require('dotenv').config();
 
-console.log(app);
+const app = express();
+
 // The standard HTTP methods
 
 // order matter
