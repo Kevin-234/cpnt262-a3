39cf0ba2b8256b0f49f698e4125b5182d266cf55
Akari Taguchi <kevintaguchi0@gmail.com>
2020-11-06 22:29:15 -0700

    add README.md

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 ## GH [repo](https://github.com/Kevin-234/cpnt262-a3)
 ## GH Pages [Demo](https://kevin-234.github.io/cpnt262-a3/)
 ## [Heroku page](https://cpnt262-a-3.herokuapp.com/)
-
+----
+I'm still working on how to appear the image on Heroku.
--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.post('/register',function(request,response){
   response.send(`<p>Thanks, ${request.body.name}! We'll send newsletter updates to ${request.body.email}.</p>`);
 });
 
+
 app.get('/',function(request,response){
   console.log(request.method);
   response.send({method: 'GET', message: 'Read Data'});
